a1e039e58ca1d323bf7525e90da5eb8ba73327f6
Arhyel Ramos Flores <arthy89>
2023-09-21 17:26:20 -0500

    dev

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -28,7 +28,7 @@
 
                 <form action="#" method="post">
                     <div class="input-group mb-3">
-                        <input type="email" class="form-control" placeholder="Email">
+                        <input type="email" class="form-control" placeholder="DNI">
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-envelope"></span>
